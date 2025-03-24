ceee6492e2d25c691c769d29cfe4f60c36b02f83
Debopriyo Basu <debopriyobasu@gmail.com>
2025-03-24 22:19:35 +0530

    initial commit

---
+++ src/main/java/in/debopriyobasu/smart_task_scheduler/SmartTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package in.debopriyobasu.smart_task_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTaskSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/debopriyobasu/smart_task_scheduler/SmartTaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.debopriyobasu.smart_task_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
