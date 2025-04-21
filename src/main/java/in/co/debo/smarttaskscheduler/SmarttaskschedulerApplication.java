96ca87574fb8190eac650d3bb709d9ced0dc92d4
Debopriyo Basu <debopriyobasu@gmail.com>
2025-04-21 19:13:06 +0530

    initial commit (gradle)

---
+++ src/main/java/in/co/debo/smarttaskscheduler/SmarttaskschedulerApplication.java
@@ -0,0 +1,13 @@
+package in.co.debo.smarttaskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarttaskschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarttaskschedulerApplication.class, args);
+	}
+
+}
